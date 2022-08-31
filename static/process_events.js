86b4419529737bdb50d0d209afe344fb86504402
scx1332 <sieciech@gmail.com>
2022-08-31 19:39:31 +0200

    fixes

--- static/process_events.js
+++ static/process_events.js
@@ -2,6 +2,15 @@ function getDateDifferenceInSecs(date1, date2) {
     return (date1.getTime() - date2.getTime()) / 1000;
 }
 
+function bytesToSize(bytes) {
+    if      (bytes >= 1073741824) { bytes = (bytes / 1073741824).toFixed(2) + " GB"; }
+    else if (bytes >= 1048576)    { bytes = (bytes / 1048576).toFixed(2) + " MB"; }
+    else if (bytes >= 1024)       { bytes = (bytes / 1024).toFixed(2) + " KB"; }
+    else if (bytes > 1)           { bytes = bytes + " bytes"; }
+    else if (bytes == 1)          { bytes = bytes + " byte"; }
+    else                          { bytes = "0 bytes"; }
+    return bytes;
+}
 
 function plotSizes(sizes) {
     let sizes_times = []
